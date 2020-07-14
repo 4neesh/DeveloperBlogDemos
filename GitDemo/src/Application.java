91dd6987bfe40a1fb1ad335b9fbc2f476196b08e
Aneesh <aneesh@sky.com>
2020-07-14 07:58:55 +0100

    first branch print line

--- GitDemo/src/Application.java
+++ GitDemo/src/Application.java
@@ -3,5 +3,6 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("Hello from the main branch");
+        System.out.println("Hello from the first branch");
     }
 }
